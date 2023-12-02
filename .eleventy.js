90ef028b4524c8ea7bd527501b9c661b72e87bb6
Veronika Stojanovová <verca.stojanovova@gmail.com>
2023-12-02 18:01:13 +0000

    done

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("css");
+
+    return {
+    templateFormats: ["njk", "html", "md"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    }
+
+};
+   
